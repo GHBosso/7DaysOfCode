bc354dc24b08eb60a44b759517fff47f41ce1b30
Guilherme Henrique Bosso Barreiro <gbossox@gmail.com>
2023-04-26 16:13:04 -0300

    criado o extrador do conteudo IMDB

--- 7DaysOfCode/src/App.java
+++ 7DaysOfCode/src/App.java
@@ -1,29 +1,31 @@
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
 import java.util.Properties;
 
 public class App {
-	
+
 	public static void main(String[] args) throws IOException {
-		
+
 		Properties prop = new Properties();
 		FileInputStream file = new FileInputStream("./properties/dados.properties");
 		prop.load(file);
 		String url = prop.getProperty("prop.url");
-		
-		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder()
-		      .uri(URI.create(url))
-		      .GET()
-		      .build();
-		client.sendAsync(request, BodyHandlers.ofString())
-		      .thenApply(HttpResponse::body)
-		      .thenAccept(System.out::println)
-		      .join();
+
+		ClienteHttp http = new ClienteHttp();
+		String json = http.buscaDados(url);
+
+		ExtratorDeConteudoDoIMDB extrator = new ExtratorDeConteudoDoIMDB();
+		List<Conteudo> conteudos = extrator.extraiConteudosIMDB(json);
+
+		for (int i = 0; i < 5; i++) {
+
+			Conteudo conteudo = conteudos.get(i);
+			System.out.println(conteudo.getTitulo());
+			System.out.println(conteudo.getUrlImagem());
+			System.out.println();
+
+		}
+
 	}
 }
